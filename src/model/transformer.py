728dba9888b234bf3743d471eb01fa46cdeed962
Dante Lokitiyakul <66824555+dante-hl@users.noreply.github.com>
2024-07-22 13:16:25 -0400

    add final linear layer on TransformerEncoder to go from representation to prediction, add PyTorchLightning functions (training/val/test_step, step, configure_optimizers) for pl compatibility, modify EncoderBlock forward to take a dictionary to work with nn.Sequential's SISO design

--- src/model/transformer.py
+++ src/model/transformer.py
@@ -3,13 +3,17 @@ import torch
 import torch.nn as nn
 import numpy as np
 import copy
+from einops.layers.torch import Rearrange, Reduce # make Transformer implementation more readable later
+import pytorch_lightning as pl
 
+PADDING_VALUE = -1e9
 # %%
-class TransformerEncoder(nn.Module):
+class TransformerEncoder(pl.LightningModule):
     """
     Transformer encoder - consists of `n_blocks` EncoderBlocks in series.
     """
-    def __init__(self, n_blocks, n_heads, d_model, d_hidden, dropout=0.1) -> None:
+    def __init__(self, n_blocks, n_heads, d_model, d_hidden, dropout: float = 0.1, lr: float = 0.001,
+                 weight_decay: float = 0.0) -> None:
         """
         Initialize transformer encoder.
 
@@ -22,6 +26,9 @@ class TransformerEncoder(nn.Module):
         self.encoder = EncoderBlock(n_heads, d_model, d_hidden, dropout)
         self.model = nn.Sequential(*[copy.deepcopy(self.encoder) for _ in range(n_blocks)])
         self._initialize_weights(self.model)
+        self.lr = lr
+        self.weight_decay = weight_decay
+        self.final_linear = nn.Linear(d_model, d_model)
 
     def _initialize_weights(self, model : nn.Module) -> None:
         for m in model.modules():
@@ -30,9 +37,42 @@ class TransformerEncoder(nn.Module):
 
     def forward(self, x, padding_mask=None) -> torch.Tensor:
         # x is (batch_size, seq_len, emb_dim)
-        x = self.model(x, padding_mask)
-        return x
+        context_dict = self.model({'data':x, 'padding_mask':padding_mask}) # dict required since nn.Sequential only takes single arguments as input
+        context = context_dict['data']
+        # take context vector at final position, apply linear to predict using final position
+        output = self.final_linear(context[:, -1, :])
+        # padding_mask = context_dict['padding_mask'] # in case you need it 
+        return output # shape (bs, 1, 1)
+    
+    # copied from other methods - modify as needed
+    def training_step(self, batch: torch.Tensor, batch_idx: int) -> torch.Tensor:
+        return self.step(batch, 'train')
 
+    def validation_step(self, batch: torch.Tensor, batch_idx: int) -> torch.Tensor:
+        return self.step(batch, 'val')
+
+    def test_step(self, batch: torch.Tensor, batch_idx: int) -> torch.Tensor:
+        return self.step(batch, 'test')
+
+    def configure_optimizers(self) -> torch.optim.Optimizer:
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.lr, weight_decay=self.weight_decay)
+        scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=self.trainer.max_epochs, eta_min=0.0)
+        return [optimizer], [scheduler]
+
+    def step(self, batch: torch.Tensor, stage: str) -> torch.Tensor:
+        """
+        Forward pass of the network.
+        Args:
+            batch: The input data.
+            stage: The stage of the network.
+        Returns:
+            The output of the network.
+        """
+        x, y = batch
+        y_hat = self(x).squeeze()
+        loss = nn.MSELoss()(y_hat, y)
+        self.log(f'{stage}_loss', loss, prog_bar=True)
+        return loss
     
 
 # %%
@@ -65,12 +105,21 @@ class EncoderBlock(nn.Module):
                                  nn.Linear(d_hidden, d_model))
         self.mlp_dropout = nn.Dropout(dropout)
 
-    def forward(self, x, padding_mask=None):
+    def forward(self, input_dict):
         """
-        input: x (batch_size, max_seq_len, d_model)
+        input: inpuct_dict, {'data': x, 'padding_mask':padding_mask}, where
+            
+            x: a (batch_size, max_seq_len, d_model) data tensor
+            padding_mask: mask to ignore padding tokens when applying softmax to attention matrix. (see MultiHeadAttention forward for more details)
+
+            inputs are packaged this way to allow EncoderBlock to be used with nn.Sequential, which expects atoms to have only a single input
+
         output: a (batch_size, max_seq_len, d_model)
-        padding_mask: mask to ignore padding tokens when applying softmax to attention matrix. (see MultiHeadAttention forward for more details)
         """
+        # extract items from dictionary
+        x = input_dict['data']
+        padding_mask = input_dict['padding_mask']
+
         # MultiHeadAttention sublayer
         z = self.attn_norm(x)
         z, _ = self.mh_attn(x, x, x, padding_mask) # out is (bs, seq, d_model)
@@ -80,9 +129,7 @@ class EncoderBlock(nn.Module):
         a = self.mlp_norm(z)
         a = self.mlp(a)
         a = z + self.mlp_dropout(a)
-        return a
-
-
+        return {'data':a, 'padding_mask':padding_mask}
 
 # %%
 class MultiHeadAttentionBlock(nn.Module):
@@ -108,7 +155,7 @@ class MultiHeadAttentionBlock(nn.Module):
 
         padding_mask: mask for ignoring certain columns of the raw attention matrix when applying softmax. 'True' corresps. to padding tokens. 
         
-        padding_mask is used to set columns corresp. to padded tokens to -1e-9, so they effectively contribute nothing to the softmax and are set to 0. this has the effect of ensuring that padded tokens are not attended to. (rows corresp. to padding are not masked, as they only need to be masked/ignored at loss calculation to ensure they have no effect on the loss and thus learning.)
+        padding_mask is used to set columns corresp. to padded tokens to -1e9, so they effectively contribute nothing to the softmax and are set to 0. this has the effect of ensuring that padded tokens are not attended to. (rows corresp. to padding are not masked, as they only need to be masked/ignored at loss calculation to ensure they have no effect on the loss and thus learning.)
         """
         # x is (batch_size, seq_len, emb_dim), where seq_len is max sequence length of all sequences in batch
         bs, seq, _ = queries.shape # batch_size, seq_len 
@@ -127,7 +174,7 @@ class MultiHeadAttentionBlock(nn.Module):
         if padding_mask is not None:
             # (bs, seq, 1) -> (bs, 1, seq, 1) for n_head dimension broadcasting
             padding_mask = padding_mask.unsqueeze(dim=1)
-            attn.masked_fill(~padding_mask, -1e9) 
+            attn.masked_fill(~padding_mask, PADDING_VALUE) 
             # see above for explanation of padding masking
 
         # normalize
@@ -146,5 +193,3 @@ class MultiHeadAttentionBlock(nn.Module):
         # send through linear layer
         out = self.proj(Z) # (bs, seq, out_dim)
         return out, attn 
-
-# %%
