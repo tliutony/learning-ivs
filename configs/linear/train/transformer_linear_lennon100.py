392538b46ca694220d86114dab57a64af36d0b78
Dante Lokitiyakul <66824555+dante-hl@users.noreply.github.com>
2024-08-26 15:56:07 -0400

    add transformer training config (transformer_linear_lennon100)

---
+++ configs/linear/train/transformer_linear_lennon100.py
@@ -0,0 +1,34 @@
+_base_ = './base.py'
+
+# model
+model = dict(name='TransformerEncoder', n_blocks=4, n_heads=4, d_model=1, d_hidden=256, pooling='average')
+
+# data
+work_dir = './workdir'
+data_dir = './tmp_lennon100/range' # activate online data generation by setting data_dir to None and specifying data_cfg
+data_cfg = None
+# prefer hf_dataset of data_dir
+hf_dataset = 'learning-ivs/lennon100-range-tau-10k'
+#data_cfg = 'datasets/linear/lennon100.py'
+train_batch_size = 256
+val_batch_size = 256
+test_batch_size = 256
+
+# optimization
+max_epochs = 100
+lr = 3e-4
+weight_decay = 1e-4
+
+# logging
+logging = True
+#exp_name = f'mlp_lennon100_bs{train_batch_size}_lr{lr}_eps{max_epochs}'x
+exp_name = f'transformer_lennon100_bs{train_batch_size}_lr{lr}_wd{weight_decay}_eps{max_epochs}'
+# exp_name = f'transformer_lennon100_lr{lr}_eps{max_epochs}'
+
+
+# logging
+logging = True
+project_name = 'iv_linear_normal'
+work_dir = './checkpoints/linear'
+early_stopping = dict(monitor='val_loss', mode='min', patience=20)
+checkpoint = dict(monitor='val_loss', mode='min', save_top_k=1)
